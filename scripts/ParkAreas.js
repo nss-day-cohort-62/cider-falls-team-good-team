cfbdf6077df3e3c348b5cab27c4ae4e30d436dd2
Jessalynn Whyte <jessalynn.whyte@gmail.com>
2023-01-30 14:02:00 -0600

    Add park areas HTML string and listener event

---
+++ scripts/ParkAreas.js
@@ -0,0 +1,49 @@
+import { getParkAreas, getGuests } from "./database";
+
+const parkAreas = getParkAreas();
+const guests = getGuests();
+
+//click event to count guests in any given park area
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  //if id created in HTML matches "parkArea" when clicked
+  if (itemClicked.id.startsWith("parkArea")) {
+    const [, parkAreaId] = itemClicked.id.split("--");
+//iterate through parkAreas
+    for (const area of parkAreas) {
+        //if PK matches integer from HTML
+      if (area.id === parseInt(parkAreaId)) {
+        //create constant to hold value created by invoking function to count guests
+        const guestsPresent = guestsInArea(parkAreas);
+        window.alert(
+            //string that tells how many guests are in any given area.
+          `${area.name} currently welcomes ${guestsPresent} guests.`
+        );
+      }
+    }
+  }
+});
+
+//create function to iterate through park areas and return string
+export const ParkAreas = (parkAreas) => {
+  //set empty HTML string
+  let parkAreaHTML = "";
+  // iterate through parkAreas variable
+  for (const area of parkAreas) {
+    parkAreaHTML += `<h2 class="headline--areas" id="parkArea--${parkAreas.id}">${parkAreas.name}</h2>`;
+  }
+  return parkAreaHTML;
+};
+
+//create function to match parkArea.id and fk on guests and return the count of all guests in an area.
+const guestsInArea = (parkArea) => {
+  //create variable set to 0
+  let guestsPresent = 0;
+
+  for (const guest of guests) {
+    if (guest.parkAreaId === parkArea.id) {
+      guestsPresent++;
+    }
+  }
+  return guestsPresent;
+};
